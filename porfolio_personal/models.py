7da4414ccee1605ad43178d1959a97362dfa5309
Mauricio Romero <116466045+Mauricio410@users.noreply.github.com>
2024-03-16 21:38:57 -0300

    Porfolio v1.13

--- porfolio_personal/models.py
+++ porfolio_personal/models.py
@@ -4,11 +4,14 @@ from django.db import models
 
 class Proyectos(models.Model):
     nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(max_length=300)
-    imagen = models.ImageField(upload_to='porfolio/images/')
+    descripcion = models.TextField(max_length=300, blank=True)
+    imagen = models.ImageField(upload_to='porfolio/images/', blank=True)
     url = models.URLField(max_length=200, blank=True)
+    fecha = models.DateField(null=True, blank=True)
+
 
 class redes(models.Model):
     id = models.BigAutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     url = models.URLField(max_length=200, blank=True)
+
